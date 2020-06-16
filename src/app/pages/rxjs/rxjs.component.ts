abcdb2e1dd9120d31833fe49c2d3c2673fd56bee
Nicolas <nicolascordobacampos@gmail.com>
2020-06-15 23:22:46 -0500

    Se avanzaron 3 clases.

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
+import { retry, map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -8,25 +9,7 @@ import { Observable } from 'rxjs';
 })
 export class RxjsComponent implements OnInit {
   constructor() {
-    const obs = new Observable((observer) => {
-      let contador = 0;
-
-      const intervalo = setInterval(() => {
-        contador += 1;
-
-        observer.next(contador);
-
-        if (contador === 3) {
-          clearInterval(intervalo);
-          observer.complete();
-        }
-        // if (contador === 2) {
-        //   observer.error('Error');
-        // }
-      }, 500);
-    });
-
-    obs.subscribe(
+    this.regresaObservable().subscribe(
       (response) => console.log(response),
       (error) => console.log(error),
       () => console.log('El observador termino.')
@@ -34,4 +17,40 @@ export class RxjsComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  regresaObservable(): Observable<any> {
+    return new Observable((observer: Subscriber<any>) => {
+      let contador = 0;
+
+      const intervalo = setInterval(() => {
+        contador += 1;
+
+        const salida = {
+          valor: contador,
+        };
+
+        observer.next(salida);
+
+        if (contador === 3) {
+          clearInterval(intervalo);
+          observer.complete();
+        }
+        // if (contador === 2) {
+        //   clearInterval(intervalo);
+        //   observer.error('Error');
+        // }
+      }, 500);
+    }).pipe(
+      map((resp) => resp.valor),
+      filter((valor, index) => {
+        if (valor % 2 === 1) {
+          // impar
+          return true;
+        } else {
+          // par
+          return false;
+        }
+      })
+    );
+  }
 }
